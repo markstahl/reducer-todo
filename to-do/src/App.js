26e54200d197c5155a57debbd30236d089608809
mark stahl <markstahl0219@gmail.com>
2020-07-22 23:59:12 -0400

    styled

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Your To Do List</h1>
+        <h1>Your To-Do List</h1>
         <TodoForm />
       </header>
     </div>
